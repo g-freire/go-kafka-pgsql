2a69c5576d522f79d93ff280db99b4e9f4899408
g.freire <gustavof@prettytechnical.io>
2021-05-30 18:44:27 -0300

    add wg group, pgx pool load test works

--- go-api/pkg/event/pg_client.go
+++ go-api/pkg/event/pg_client.go
@@ -6,16 +6,17 @@ import (
 	db "event-driven/internal/db/postgres"
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
 )
 
 
-func insertLoad(client *sql.DB, clientN string) {
-	for{
+func insertLoad(client *sql.DB, clientN string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
 		msg := time.Now().String()
 		sqlStatement := `INSERT INTO KAFKA (value) VALUES ($1)`
 		_, err := client.Exec(sqlStatement, msg)
@@ -25,11 +26,11 @@ func insertLoad(client *sql.DB, clientN string) {
 			fmt.Print("\n INSERTED ", clientN, " ", msg)
 		}
 	}
-
 }
 
-func insertLoadPool(connection *pgxpool.Pool, clientN string) {
-	for{
+func insertLoadPool(connection *pgxpool.Pool, clientN string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
 		msg := time.Now().String()
 		sqlStatement := `INSERT INTO KAFKA (value) VALUES ($1)`
 		//
@@ -37,66 +38,64 @@ func insertLoadPool(connection *pgxpool.Pool, clientN string) {
 		//if err != nil {
 		//	panic(err)
 		//} else {
-			//defer conn.Release()
-			_, err := connection.Exec(context.Background(), sqlStatement, msg)
-			if err != nil {
-				panic(err)
-			} else {
-				fmt.Print("\n INSERTED ", clientN, " ", msg)
-			}
+		//defer conn.Release()
+		_, err := connection.Exec(context.Background(), sqlStatement, msg)
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Print("\n INSERTED ", clientN, " ", msg)
 		}
 	}
+}
 
-
-	//_, err = connection.Exec(context.Background(), sqlStatement, msg)
-	//if err != nil {
-	//	panic(err)
-	//} else {
-	//	fmt.Print("\n INSERTED ", clientN, " ", msg)
-	//}
-
-
+//_, err = connection.Exec(context.Background(), sqlStatement, msg)
+//if err != nil {
+//	panic(err)
+//} else {
+//	fmt.Print("\n INSERTED ", clientN, " ", msg)
+//}
 // load test the db with many inserts
 
 func StartLoadTest(defaultPostgresURI string) {
 	client := db.NewPostgresSingletonClient(defaultPostgresURI)
 	defer client.Close()
 
-	for i := 0; i<10 ; i++{
+	var wg sync.WaitGroup
+	for i := 0; i < 500; i++ {
 		s := strconv.Itoa(i)
-		go insertLoad(client, s)
-		go insertLoad(client, s)
-		go insertLoad(client, s)
-
-		//time.Sleep(time.Duration(1) * time.Second)
+		wg.Add(1)
+		//go insertLoad(client, s, &wg)
+		go insertLoad(client, s, &wg)
 	}
-	time.Sleep(time.Duration(1) * time.Second)
+	wg.Wait()
 
 }
 
 func StartLoadTestPool(postgresURI string) {
 	client := db.NewPostgresConnectionPool(postgresURI)
 	defer client.Close()
-	for i := 0; ; i++{
+	var wg sync.WaitGroup
+	for i := 0; i < 500000; i++ {
 		s := strconv.Itoa(i)
-		go insertLoadPool(client, s)
+		wg.Add(1)
+		go insertLoadPool(client, s, &wg)
 		//time.Sleep(time.Duration(1) * time.Second)
 	}
+	wg.Wait()
 }
 
 // load test the db session with many inserts
 
-
-func StartLoadTestSessions(defaultPostgresURI string) {
-	postgresURI := os.Getenv("DATABASE_URI")
-	if postgresURI == "" {
-		postgresURI = defaultPostgresURI
-	}
-	client := db.NewPostgresConnectionPool(postgresURI)
-	defer client.Close()
-	//load test the db session limit
-	for i := 0; i < 5000; i++ {
-		s := strconv.Itoa(i)
-		insertLoadPool(client, s)
-	}
-}
+//func StartLoadTestSessions(defaultPostgresURI string) {
+//	postgresURI := os.Getenv("DATABASE_URI")
+//	if postgresURI == "" {
+//		postgresURI = defaultPostgresURI
+//	}
+//	client := db.NewPostgresConnectionPool(postgresURI)
+//	defer client.Close()
+//	//load test the db session limit
+//	for i := 0; i < 5000; i++ {
+//		s := strconv.Itoa(i)
+//		insertLoadPool(client, s)
+//	}
+//}
